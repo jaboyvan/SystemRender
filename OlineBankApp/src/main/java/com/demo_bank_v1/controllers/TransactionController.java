52670071432582b99c66eb6a7d3ee2f53a6b027f
jabo yvan <jaboyvan01@gmail.com>
2023-12-16 10:03:18 +0200

    spring

---
+++ OlineBankApp/src/main/java/com/demo_bank_v1/controllers/TransactionController.java
@@ -0,0 +1,31 @@
+package com.demo_bank_v1.controllers;
+
+import com.demo_bank_v1.service.TransactionHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    private final TransactionHistoryService transactHistoryService;
+
+    @Autowired
+    public TransactionController(TransactionHistoryService transactHistoryService) {
+        this.transactHistoryService = transactHistoryService;
+    }
+    @GetMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteTransactionRecords(@PathVariable("userId") String userId) {
+        try {
+            int userIdInt = Integer.parseInt(userId);
+            transactHistoryService.deleteTransactionRecordsById(userIdInt);
+            return ResponseEntity.ok("Transaction records deleted successfully!");
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid user ID format");
+        }
+    }
+}
+
---
+++ OlineBankApp/src/main/java/com/demo_bank_v1/service/TransactionHistoryService.java
@@ -0,0 +1,49 @@
+package com.demo_bank_v1.service;
+
+import com.demo_bank_v1.models.TransactionHistory;
+import com.demo_bank_v1.repository.TransactHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionHistoryService {
+
+    private final TransactHistoryRepository transactHistoryRepository;
+
+    @Autowired
+    public TransactionHistoryService(TransactHistoryRepository transactHistoryRepository) {
+        this.transactHistoryRepository = transactHistoryRepository;
+    }
+
+    public List<TransactionHistory> getTransactionRecordsById(int userID) {
+        return transactHistoryRepository.getTransactionRecordsById(userID);
+    }
+
+    public void deleteTransactionRecordsById(int userID) {
+        transactHistoryRepository.deleteTransactionRecordsById(userID);
+    }
+
+
+
+//    public void updateTransactionRecordsById(
+//            int userId,
+//            String transactionType,
+//            BigDecimal amount,
+//            String source,
+//            String status,
+//            String reasonCode,
+//            LocalDateTime createdAt
+//    ) {
+//        transactHistoryRepository.updateTransactionRecordsById(
+//                userId,
+//                transactionType,
+//                amount,
+//                source,
+//                status,
+//                reasonCode,
+//                createdAt
+//        );
+    }
+
